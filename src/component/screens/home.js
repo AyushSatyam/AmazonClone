b6db54c952d2d83bffc830dc582eb268f20047db
AyushSatyam <42187810+AyushSatyam@users.noreply.github.com>
2021-06-04 23:26:49 +0530

    Carousel with image backgroud and Card on the home page is added with login button in card

--- src/component/screens/home.js
+++ src/component/screens/home.js
@@ -5,8 +5,45 @@ import {
   CarouselControl,
   CarouselIndicators,
   CarouselCaption,
+  Row,
+  Col,
 } from "reactstrap";
+import HomeCard from "./homeCard";
 // import "../images";
+const cardText = [
+  {
+    id: 1,
+    altText: "Card 1",
+    img: "https://images.unsplash.com/photo-1501163268664-3fdf329d019f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGVsZWN0cm9uaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    text: "Up to 70% off | Electronics clearance store",
+    button: false,
+    linkText: "See more"
+  },
+  {
+    id: 2,
+    altText: "Card 2",
+    img: "https://images.unsplash.com/photo-1543168256-418811576931?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Z3JvY2VyeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    text: "Everyday essentials",
+    button: false,
+    linkText: "See more"
+  },
+  {
+    id: 3,
+    altText: "Card 3",
+    img: "https://images.unsplash.com/photo-1501163268664-3fdf329d019f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGVsZWN0cm9uaWN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    text: "Styles for Men | Up to 70% off",
+    button: false,
+    linkText: "View all offers"
+  },
+  {
+    id: 4,
+    altText: "",
+    img: "",
+    text: "Sign in for your best experience",
+    button: true,
+    linkText: ""
+  },
+];
 const items = [
   {
     id: 1,
@@ -22,6 +59,26 @@ const items = [
     id: 3,
     altText: "Slide 3",
     img: "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/OnePlusNordNewLaunch/2ndJune/GW/Revised/Nord_June_2CS_1500x600._CB667250719_.jpg",
+  },
+  {
+    id: 4,
+    altText: "Slide 4",
+    img: "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Gateway/Flip/Essentials_Experience_May21/Revised_EE_Graphics/GW_PC_BUNK_1500x600._CB668893605_.jpg",
+  },
+  {
+    id: 5,
+    altText: "Slide 5",
+    img: "https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/SingleTitle/DOM/Launch/1500x600_Hero-Tall_JPN._CB667281672_.jpg",
+  },
+  {
+    id: 6,
+    altText: "Slide 6",
+    img: "https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Avatar/HeroPC_1500x600_SVA._CB667240774_.jpg",
+  },
+  {
+    id: 7,
+    altText: "Slide 7",
+    img: "https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/iQoo/iQOO_Z3/GW/1stJune/D23951818_WLM_iQOO_Z3_Launch_GW_Design_SIM_TallHero_1500x600._CB667332720_.jpg",
   },
 ];
 
@@ -50,7 +107,7 @@ const Home = (props) => {
     return (
       <CarouselItem
         className="custom-tag"
-        tag="div"
+        // tag="div"
         key={item.id}
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
@@ -62,31 +119,55 @@ const Home = (props) => {
 
   return (
     <div>
-      <style>
-        {`.custom-tag {
-              max-width: 100%;
-              height: 500px;
-              background: black;
-            }`}
-      </style>
-      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-        <CarouselIndicators
-          items={items}
-          activeIndex={activeIndex}
-          onClickHandler={goToIndex}
-        />
-        {slides}
-        <CarouselControl
-          direction="prev"
-          // directionText="Previous"
-          onClickHandler={previous}
-        />
-        <CarouselControl
-          direction="next"
-          // directionText="Next"
-          onClickHandler={next}
-        />
-      </Carousel>
+      <div>
+        <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+          <CarouselIndicators
+            items={items}
+            activeIndex={activeIndex}
+            onClickHandler={goToIndex}
+          />
+          {slides}
+          <CarouselControl
+            direction="prev"
+            // directionText="Previous"
+            onClickHandler={previous}
+          />
+          <CarouselControl
+            direction="next"
+            // directionText="Next"
+            onClickHandler={next}
+          />
+        </Carousel>
+      </div>
+      <Row>
+        {cardText.map((cardTextItem) => {
+          return (
+            <Col>
+              <div className="home_card">
+                <HomeCard
+                  key={cardTextItem.id}
+                  altText={cardTextItem.altText}
+                  text={cardTextItem.text}
+                  img={cardTextItem.img}
+                  button={cardTextItem.button}
+                  linkText={cardTextItem.linkText}
+                />
+              </div>
+            </Col>
+          );
+        })}
+
+        {/* <Col sm="4">
+          <div className="home_card">
+            <HomeCard />
+          </div>
+        </Col>
+        <Col sm="4">
+          <div className="home_card">
+            <HomeCard />
+          </div>
+        </Col> */}
+      </Row>
     </div>
   );
 };
---
+++ src/component/screens/homeCard.js
@@ -0,0 +1,78 @@
+// import React from 'react'
+
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  CardLink,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./login/login";
+// import "style-loader/css-loader/bootstrap/dist/css/bootstrap.css";
+// const Example = (props) => {
+//   return (
+
+//   );
+// };
+
+function HomeCard(props) {
+  return (
+    <Card className="home_card_card">
+      <CardBody>
+        <CardTitle
+          tag="h4"
+          style={{ fontWeight: "bold", marginBottom: "15px" }}
+        >
+          {props.text}
+        </CardTitle>
+        {/* <CardSubtitle tag="h6" className="mb-2 text-muted">
+            Card subtitle
+          </CardSubtitle> */}
+        <CardImg
+          width="100%"
+          src={props.img}
+          alt={props.altText}
+          style={{ zIndex: "10" }}
+        />
+      </CardBody>
+
+      {/* <CardText>
+            
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </CardText> */}
+      {/* <Button>Button</Button> */}
+      <CardBody>
+        {props.button === true ? (
+          // <Button></Button>
+          // <Button
+          //   className="form_button"
+          //   size="sm"
+          //   type="submit"
+          //   // block
+          //   onClick={<Login />}
+          // >
+          //   Sign in securely
+          // </Button>
+          <CardLink
+            href="/login"
+            className=" btn form_button"
+            //  style={{ textDecoration: "none" }}
+          >
+            Sign in securely
+          </CardLink>
+        ) : (
+          <CardLink href="#" style={{ textDecoration: "none" }}>
+            {props.linkText}
+          </CardLink>
+        )}
+      </CardBody>
+    </Card>
+  );
+}
+export default HomeCard;
